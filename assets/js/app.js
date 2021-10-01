90eeb0ebc90ff5f645de23347e61af4c8cc6cacb
Nicols <nicole.gamboacifuentes@gmail.com>
2021-10-01 19:00:01 -0300

    agregar ancla

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,40 @@
-$(".iconos").click(function(){
-    $("p").toggle();
-})
+$(document).ready(function(){
+    console.log( "ready!" );
+
+
+    $(".iconos").click(function(){
+        $("p").toggle();
+    })
+    
+    $('#navInicio').click(function(event) {
+        event.preventDefault();
+        document.querySelector('#inicio').scrollIntoView({
+            behavior: 'smooth'
+        })
+    })
+    
+    $('#navQuienesSomos').click(function(event) {
+        event.preventDefault();
+        document.querySelector('#quienesSomos').scrollIntoView({
+            behavior: 'smooth'
+        })
+    })
+    
+    $('#navDestacados').click(function(event) {
+        event.preventDefault();
+        document.querySelector('#destacados').scrollIntoView({
+            behavior: 'smooth'
+        })
+    })
+    
+    $('#navContacto').click(function(event) {
+        event.preventDefault();
+        document.querySelector('#contacto').scrollIntoView({
+            behavior: 'smooth'
+        })
+    })
+    
+
+
+});
+
