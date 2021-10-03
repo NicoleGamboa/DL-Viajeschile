fef399332196f6d8e22940fcba977495044a51e7
Nicols <nicole.gamboacifuentes@gmail.com>
2021-10-02 21:56:50 -0300

    agregar comentarios y boton a la sección de destacados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
     console.log( "ready!" );
 
-
+    // alternar entre mostrar y ocultar parrafos asociados a los iconos
     $(".iconos").click(function(){
-        $("p").toggle();
+        $(".parrafo-icono").toggle();
     })
     
+    // hacer navegacion al clic -> llevar a seccion de inicio
     $('#navInicio').click(function(event) {
         event.preventDefault();
         document.querySelector('#inicio').scrollIntoView({
             behavior: 'smooth'
         })
     })
-    
+
+    // hacer navegacion al clic -> llevar a seccion de quienesSomos
     $('#navQuienesSomos').click(function(event) {
         event.preventDefault();
         document.querySelector('#quienesSomos').scrollIntoView({
@@ -20,21 +22,29 @@ $(document).ready(function(){
         })
     })
     
+    // hacer navegacion al clic -> llevar a seccion de destacados
     $('#navDestacados').click(function(event) {
         event.preventDefault();
         document.querySelector('#destacados').scrollIntoView({
             behavior: 'smooth'
         })
-    })
+    });
     
+    // hacer navegacion al clic -> llevar a seccion de contacto
     $('#navContacto').click(function(event) {
         event.preventDefault();
         document.querySelector('#contacto').scrollIntoView({
             behavior: 'smooth'
         })
-    })
+    });
     
-
+    // hacer navegacion al clic del botón -> llevar a seccion de contacto
+    $('#botonAlerta').click(function(event) {
+        event.preventDefault();
+        document.querySelector('#contacto').scrollIntoView({
+            behavior: 'smooth'
+        })
+    });
 
 });
 
